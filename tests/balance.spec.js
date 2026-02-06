5dbb2b56894ef05b841c2b7a6c12b194de4d9917
MartinGruter <Martin.Grter@iths.se>
2026-02-06 14:55:51 +0100

    Lagt till testeer

--- tests/balance.spec.js
+++ tests/balance.spec.js
@@ -2,12 +2,12 @@ import {expect, test} from '@playwright/test';
 
 test('Testa att balance går att nå', async ({page}) => {
     const response = await page.goto('http://localhost:8080/balance');
+    expect(response).not.toBeNull();
     expect(response.status()).toBe(200);
 });
 
 test('Testa att sidan laddas korrekt', async ({page}) => {
     await page.goto('http://localhost:8080/balance');
-
     await expect(page.locator('h1')).toHaveText('Saldo');
 });
 
